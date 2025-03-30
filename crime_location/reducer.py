ed0b03eb0c1775d07c6a00e37978e7b14b2e9b49
Joshi B <dev.bhargavj@gmail.com>
2025-03-31 05:26:59 +0530

    Update reducer.py

--- crime_location/reducer.py
+++ crime_location/reducer.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 import sys
-from collections import defaultdict
 
-crime_counts = defaultdict(int)
+crime_counts = {}
 
 for line in sys.stdin:
     location, count = line.strip().split("\t")  # Read key-value pair
-    crime_counts[location] += int(count)  # Sum up crime occurrences
+    count = int(count)
+
+    if location in crime_counts:
+        crime_counts[location] += count  # Update count
+    else:
+        crime_counts[location] = count  # Initialize count
 
 # Output sorted crime hotspots
 for location, total in sorted(crime_counts.items(), key=lambda x: x[1], reverse=True):  # Sort by highest crime count
