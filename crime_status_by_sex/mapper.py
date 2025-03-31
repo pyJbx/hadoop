4ce8084043519614cf8d9c9231865a7d51dc54f7
Joshi B <dev.bhargavj@gmail.com>
2025-03-31 05:47:59 +0530

    Update mapper.py

--- crime_status_by_sex/mapper.py
+++ crime_status_by_sex/mapper.py
@@ -8,5 +8,5 @@ for line in sys.stdin:
     status = fields[18]  # Status Desc (Crime Status)
 
     # Validate data (ignore missing or incorrect values)
-    if vict_sex in ["M", "F", "X"]:
+    if vict_sex and status and vict_sex in {"M", "F", "X"}:
         print(f"{status}\t{vict_sex}\t1")  # Emit key-value pair with count
